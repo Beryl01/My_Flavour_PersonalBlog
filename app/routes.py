7c433dc6a7e104e7745e8c1bdcdf293060907f02
Beryl01 <berylnegesa@gmail.com>
2020-05-10 18:10:05 +0300

    First Deploy Trial

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,6 @@ def index():
 @app.route("/view")
 @login_required
 def view():
-   
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=5)
     return render_template('view.html', posts=posts)          
