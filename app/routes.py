9fa805a23d6cd2ad6b718771279a32156a18f447
Beryl01 <berylnegesa@gmail.com>
2020-05-10 17:27:09 +0300

    Worked on route  for new blogs

--- app/routes.py
+++ app/routes.py
@@ -72,3 +72,21 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form) 
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_pitch.html', title='Update Post', form=form, legend='Update Post')
